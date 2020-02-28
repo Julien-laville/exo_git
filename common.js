59c2e7c7fd311630c52c783474a8a29ef1a647e7
Julien Laville <julien.laville@jobteaser.com>
2020-02-28 17:35:24 +0100

    fix vector

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
-
-function cross(vector) {
-  return this.x * vector.y + thix.y * vector.x
+// i prefer vec a vec b to avoid using call
+function cross(vectA, vectB) {
+  return vectA.x * vectB.y + vectA.y * vectB.x // go to common branch
 }
 
